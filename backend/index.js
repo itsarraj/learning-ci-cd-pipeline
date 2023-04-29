f00415df625787085d651e56337afa0c82a81545
Restyled.io <commits@restyled.io>
2023-04-29 16:59:09 +0000

    Restyled by clang-format

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const fs = require("fs");
 
 function requestHandler(req, res) {
   console.log(req.url);
-  res.writeHead(200, { "content-type": "text/html" });
-  fs.readFile('./index.html' , function(err , data) {
+  res.writeHead(200, {"content-type" : "text/html"});
+  fs.readFile('./index.html', function(err, data) {
     if (err) {
-      console.log('error' , err);
+      console.log('error', err);
       return res.end('<h1>404 Not Found</h1>');
     }
     res.end(data);
@@ -18,9 +18,10 @@ function requestHandler(req, res) {
 
 const server = http.createServer(requestHandler);
 
-server.listen(port, function (err) {
+server.listen(port, function(err) {
   if (err) {
     console.log(err);
     return;
-  } else console.log("Server listening on port ", port);
+  } else
+    console.log("Server listening on port ", port);
 });
